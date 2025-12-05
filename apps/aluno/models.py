44779de1012c63851a28827f63aef7740249a4f4
Thiago-Coutinh0 <thiagosilvaa11c@gmail.com>
2025-12-05 15:00:06 -0300

    first commit

---
+++ apps/aluno/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Aluno (models.Model):
+    nome = models.CharField(max_length=255, null=False, blank=False)
+    matricula = models.CharField(max_length=18, null=False, blank=False, unique=True)
+    telefone = models.CharField(max_length=20, null=False, blank=False)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+    
